0d02126307b1dd6f9690863e416a3cc7cd3de4e3
Schnippen <dfojcik35@gmail.com>
2024-04-26 01:46:51 +0200

    calc

--- Functions/wallCalculation.tsx
+++ Functions/wallCalculation.tsx
@@ -24,22 +24,26 @@ const finishedWall = useSelector(
   (state: RootState) => state.wallReducer.wallTilesArray,
 );
 /*   const allWallTiles = 136;
-  const EastPlayer = 34;
-  const SouthPlayer = 34;
-  const WestPlayer = 34;
-  const NorthPlayer = 34; */ //should it be hardcoded?
+  const EastPlayer = 34; //1 index=2   5=index=10  9=index=18
+  const SouthPlayer = 34; //2 index=4  6=index=12  10=index=20
+  const WestPlayer = 34;  //3=index=6  7=index=14  11=index=22
+  const NorthPlayer = 34; */ //4=index=8 8=index=16 12=index=24
+//should it be hardcoded?
 const EastWall = finishedWall.slice(0, 34);
 const SouthWall = finishedWall.slice(34, 68);
 const WestWall = finishedWall.slice(68, 102);
 const NorthWall = finishedWall.slice(102, 136);
 const dispatch = useDispatch();
-
-function WallCalculation() {
-  dispatch(setWallFragment({direction: 'east', tiles: EastWall}));
+/*   dispatch(setWallFragment({direction: 'east', tiles: EastWall}));
   dispatch(setWallFragment({direction: 'south', tiles: SouthWall}));
   dispatch(setWallFragment({direction: 'west', tiles: WestWall}));
-  dispatch(setWallFragment({direction: 'north', tiles: NorthWall}));
+  dispatch(setWallFragment({direction: 'north', tiles: NorthWall})); */
+function WallCalculation() {
   const DICE_ROLL = Math.floor(Math.random() * 12) + 1;
+  //if DICE_ROLL === 1 east
+  //shit.concat(wall.slice(wall.length-12),wall.slice(0,2))  => Dead Wall
+
+  //phase of taking tile for hand ((4*3)+2) +((4*3)+1)*3
 
   checkDiceRoll(DICE_ROLL);
 
