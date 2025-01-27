f840807e8918717dd832cb87a680d6ee766f2254
Schnippen <dfojcik35@gmail.com>
2025-01-27 21:59:44 +0100

    added debounce for ButtonSteal

--- Components/Buttons/ButtonSteal/ButtonSteal.tsx
+++ Components/Buttons/ButtonSteal/ButtonSteal.tsx
@@ -1,7 +1,15 @@
 import {Button} from '@rneui/themed';
-import React from 'react';
+import React, {useRef} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-
+function debounce(func: () => void, wait: number) {
+  let timeout: NodeJS.Timeout | null = null;
+  return () => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func();
+    }, wait);
+  };
+}
 const ButtonShape = ({
   text,
   firstColor,
@@ -15,7 +23,7 @@ const ButtonShape = ({
 }) => {
   return (
     <Button
-      onPress={handlePress} // Pass handlePress directly
+      onPress={handlePress}
       radius={'md'}
       buttonStyle={{height: 40, width: 110}}
       titleStyle={{
@@ -36,39 +44,45 @@ const ButtonShape = ({
 };
 
 export const ButtonRIICHI = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="RIICHI"
       firstColor="#f6d106"
       secondColor="#cc7000"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed Riichii');
       }}
     />
   );
 };
 export const ButtonPON = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="PON"
       firstColor="#48cae4"
       secondColor="#023e8a"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed PON');
       }}
     />
   );
 };
 export const ButtonPASS = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="PASS"
       firstColor="#9dc1db"
       secondColor="#688aa5"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed PASS');
       }}
     />
@@ -80,52 +94,59 @@ const ButtonCANCEL = () => {
   );
 };
 export const ButtonCHII = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="CHII"
       firstColor="#19e09f"
       secondColor="#12bf63"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed CHII');
       }}
     />
   );
 };
 export const ButtonKAN = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="KAN"
       firstColor="#be95c4"
       secondColor="#5e548e"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed KAN');
       }}
     />
   );
 };
 export const ButtonRON = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
+
   return (
     <ButtonShape
       text="RON"
       firstColor="#dc2f02"
       secondColor="#fb8b24"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed RON');
       }}
     />
   );
 };
 export const ButtonTSUMO = ({handlePress}: {handlePress: () => void}) => {
+  const debouncedHandlePress = useRef(debounce(handlePress, 1000)).current;
   return (
     <ButtonShape
       text="TSUMO"
       firstColor="#dc2f02"
       secondColor="#fb8b24"
       handlePress={() => {
-        handlePress();
+        debouncedHandlePress();
         console.log('pressed TSUMO');
       }}
     />
