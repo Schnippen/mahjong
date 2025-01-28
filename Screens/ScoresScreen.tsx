ea1796e2213402b0a04cb78ebe130848e4601459
Schnippen <dfojcik35@gmail.com>
2025-01-28 17:53:25 +0100

    deleted unused files and functions, replaced with improved versions.

---
+++ Screens/ScoresScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const ScoresScreen = () => {
+  return (
+    <View>
+      <Text>ScoresScreen TODO add table from npm,</Text>
+    </View>
+  );
+};
