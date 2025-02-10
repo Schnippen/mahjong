ead63c5f18c9cbea037cf8fec754d7aaa2cb63cb
Schnippen <dfojcik35@gmail.com>
2025-02-10 22:58:03 +0100

    Added a helper function to scale text within the container

---
+++ Functions/utils/getFontSize.ts
@@ -0,0 +1,7 @@
+import {Dimensions} from 'react-native';
+
+export const getFontSize = (baseFontSize: number) => {
+  const {fontScale} = Dimensions.get('screen');
+  fontScale !== 1;
+  return baseFontSize / (fontScale !== 1 ? fontScale * 1.1 : fontScale); // Increase division factor
+};
