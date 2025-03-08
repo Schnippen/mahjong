0a9446760eb74c8010aaa207f54e752acbc261ea
Schnippen <dfojcik35@gmail.com>
2025-03-08 15:50:34 +0100

    update

--- App.tsx
+++ App.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import store from './Store/store';
 import {Provider} from 'react-redux';
 import MainApp from './MainApp';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 function App(): React.JSX.Element {
-  const queryClient = new QueryClient();
-
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
         <MainApp />
-      </QueryClientProvider>
     </Provider>
   );
 }
